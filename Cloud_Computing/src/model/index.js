611af977f3a1a1a940fc90abbc1632439370d388
jesslynowen7 <owenjesslyn@gmail.com>
2023-06-12 03:25:10 +0700

    add API search

--- Cloud_Computing/src/model/index.js
+++ Cloud_Computing/src/model/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getRecipe, predict } = require('./modelController');
+const { getRecipe, predict, search } = require('./modelController');
 
 // Get Recipe
 router.get('/getRecipe', getRecipe);
+
+// Search
+router.get('/search', search);
 
 // Predict
 router.post('/predict', predict);
